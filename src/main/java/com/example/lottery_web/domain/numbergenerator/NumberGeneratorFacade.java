d740fa875af1d08107e63eb2adb973663e49f8e6
Mateusz Salamon <msalamon249@gmail.com>
2023-08-05 11:40:33 +0200

    added randomgenerator module

--- src/main/java/com/example/lottery_web/domain/numbergenerator/NumberGeneratorFacade.java
+++ src/main/java/com/example/lottery_web/domain/numbergenerator/NumberGeneratorFacade.java
@@ -1,4 +1,45 @@
 package com.example.lottery_web.domain.numbergenerator;
 
+import com.example.lottery_web.domain.numbergenerator.dto.WinningNumbersDto;
+import com.example.lottery_web.domain.numberreciever.NumberRecieverFacade;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@AllArgsConstructor
 public class NumberGeneratorFacade {
+
+    private final RandomNumberGenerable winningNumberGenerator;
+    private final WinningNumberValidator winningNumberValidator;
+    private final WinningNumbersRepository winningNumbersRepository;
+    private final NumberRecieverFacade numberRecieverFacade;
+
+
+    public WinningNumbersDto generateWinningNumbers() {
+        LocalDateTime nextDrawDate = numberRecieverFacade.retriveveNextDrawDate();
+        Set<Integer> winningNumbers = winningNumberGenerator.generateSixRandomNumbers();
+        winningNumberValidator.validate(winningNumbers);
+        winningNumbersRepository.save(WinningNumbers.builder()
+                .date(nextDrawDate)
+                .winningNumbers(winningNumbers)
+                .build());
+        return WinningNumbersDto.builder()
+                .winningNumbers(winningNumbers)
+                .build();
+    }
+
+    public WinningNumbersDto retrieveWinningNumberByDate(LocalDateTime date) {
+        WinningNumbers numbersByDate = winningNumbersRepository.findNumbersByDate(date)
+                .orElseThrow(() -> new WinningNumbersNotFoundException("Not Found"));
+        return WinningNumbersDto.builder()
+                .winningNumbers(numbersByDate.winningNumbers())
+                .date(numbersByDate.date())
+                .build();
+    }
+
+    public boolean areWinningNumbersGeneratedByDate() {
+        LocalDateTime nextDrawDate = numberRecieverFacade.retrieveNextDrawDate();
+        return winningNumbersRepository.existsByDate(nextDrawDate);
+    }
 }
---
+++ src/main/java/com/example/lottery_web/domain/numbergenerator/RandomNumberGenerable.java
@@ -0,0 +1,8 @@
+package com.example.lottery_web.domain.numbergenerator;
+
+import java.util.Set;
+
+public interface RandomNumberGenerable {
+
+    Set<Integer> generateSixRandomNumbers();
+}
